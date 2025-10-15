8e766b6c4447c367f5163d6939d3048a620309a2
Randa-hb10 <rm-400000@hotmail.com>
2025-10-15 20:57:00 +0000

    task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program prints the alphabets in lowercase excpet q and e  using puchar'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c == 'q' || c == 'e')
+		{
+			c++;
+			continue;
+		}
+		
+		putchar(c);
+		c++;
+	}
+
+	putchar ('\n');
+
+	return (0);
+
+}
+
