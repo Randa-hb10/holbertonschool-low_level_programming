26f638627174917b9738f8e618eccc9777b41821
Randa-hb10 <rm-400000@hotmail.com>
2025-10-15 20:32:33 +0000

    task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program prints the alphabets in lowercase and upper case using puchar'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+        char c = 'a';
+        char cc = 'A';
+
+        while (c <= 'z')
+        {
+                putchar(c);
+                c++;
+        }
+
+        while (cc <= 'Z')
+        {
+                putchar(cc);
+                cc++;
+        }
+
+        putchar ('\n');
+
+        return (0);
+
+}
+
