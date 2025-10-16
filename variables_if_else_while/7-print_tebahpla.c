43e572739befcf4d5061986832a61665b6b7d0f5
Randa-hb10 <rm-400000@hotmail.com>
2025-10-16 14:34:33 +0000

    Task 7

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Description: 'the program prints the alphabets in lowercase using puchar'
-*
-* Return: Always 0 (Success)
-*/
-
+ * main - prints the lowercase alphabet in reverse
+ *
+ * Return: 0
+ */
 int main(void)
 {
-
 	int ch;
-       for (ch = 'z'; ch >= 'a'; ch--)
+
+	for (ch = 'z'; ch >= 'a'; ch--)
 	{
 		putchar(ch);
-
 	}
-
-	putchar ('\n');
-
+	putchar('\n');
 	return (0);
-
 }
 
