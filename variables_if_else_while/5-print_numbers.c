e25bc3f5babccb1cd054c253109577b45b62914b
Randa-hb10 <rm-400000@hotmail.com>
2025-10-16 12:52:24 +0000

    Task5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point 
+ *
+ * Description: ' the program print all single digit numbers '
+ *
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+	int c = '0';
+	while (c <= '9')
+	{
+		printf("%c", c);
+		c++;
+	}
+	printf ("\n");
+	return (0);
+}
+
