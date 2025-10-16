3d83fdaa0fdcda75779f6097888ec6ee1e36d8fe
Randa-hb10 <rm-400000@hotmail.com>
2025-10-16 21:34:35 +0000

    task0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * descripiton: print putchar
+ *
+ * Return: always 0 (success)
+ *
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
+
