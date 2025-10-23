8dd1e508b3631501fffcb70970410922ea36469c
Randa-hb10 <rm-400000@hotmail.com>
2025-10-23 15:32:50 +0000

    done 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: string to encode
+ *
+ * Description: Letters a/A->4, e/E->3, o/O->0, t/T->7, l/L->1.
+ * You can only use one if and two loops.
+ * Return: pointer to the resulting string
+ */
+char *leet(char *str)
+{
+int i, j;
+char letters[] = "aAeEoOtTlL";
+char numbers[] = "4433007711";
+
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; letters[j] != '\0'; j++)
+{
+if (str[i] == letters[j])
+{
+str[i] = numbers[j];
+break;
+}
+}
+}
+
+return (str);
+}
