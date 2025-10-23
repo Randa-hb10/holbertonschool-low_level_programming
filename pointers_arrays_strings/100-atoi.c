bb61a2f40b92fa238a863c7f95da8b2e3848a362
Randa-hb10 <rm-400000@hotmail.com>
2025-10-23 12:26:36 +0000

    TASK 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,10 +8,8 @@
  */
 int _atoi(char *s)
 {
-int i = 0;
-int sign = 1;
-int result = 0;
-int found_num = 0;
+int i = 0, sign = 1, result = 0;
+unsigned int num = 0;
 
 while (s[i] != '\0')
 {
@@ -19,12 +17,13 @@ if (s[i] == '-')
 sign *= -1;
 else if (s[i] >= '0' && s[i] <= '9')
 {
-found_num = 1;
-result = (result * 10) + (s[i] - '0');
+num = (num * 10) + (s[i] - '0');
 }
-else if (found_num == 1)
+else if (num > 0)
 break;
 i++;
 }
-return (result *sign);
+
+result = num *sign;
+return (result);
 }
