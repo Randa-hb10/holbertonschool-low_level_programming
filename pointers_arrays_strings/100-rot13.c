d4858f0fd50f60c164b10d10720380a809e192c0
Randa-hb10 <rm-400000@hotmail.com>
2025-10-26 16:04:28 +0000

    fix root 13

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+/**
+* rot13 - Encodes a string using ROT13
+* @s: The string to encode
+*
+* Return: Pointer to the encoded string
+*
+* Description: This function takes a string and encodes it using the
+* ROT13 substitution cipher, which replaces each letter with the
+* letter 13 positions ahead in the alphabet.
+*/
+
 char *rot13(char *s)
 {
 int i, j;
