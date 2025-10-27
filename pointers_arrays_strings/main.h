d9e2de8f75a4f235b4261c1c220a84d15c2ab38b
Randa-hb10 <rm-400000@hotmail.com>
2025-10-27 12:18:57 +0000

    strchr

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -26,5 +26,6 @@ void print_number(int n);
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif
