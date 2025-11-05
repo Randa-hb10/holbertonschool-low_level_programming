607960a65db1f1d141ff22d277dc295a1e9ce649
Randa-hb10 <rm-400000@hotmail.com>
2025-11-05 18:59:36 +0000

    fix Task1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -17,11 +17,14 @@ if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-len1 = strlen(s1);
-len2 = strlen(s2);
+while (s1[len1] != '\0')
+len1++;
+while (s2[len2] != '\0')
+len2++;
+
 if (n >= len2)
 n = len2;
-concat = malloc(sizeof(char) * (len1 + n + 1));
+concat = malloc((len1 + n + 1) * sizeof(char));
 if (concat == NULL)
 return (NULL);
 for (i = 0; i < len1; i++)
