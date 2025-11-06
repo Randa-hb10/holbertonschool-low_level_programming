2023b80d9a1bf2a612c133289d2f4281e89dadb6
Randa-hb10 <rm-400000@hotmail.com>
2025-11-06 14:41:43 +0000

    new type dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,6 +15,9 @@ char *name;
 float age;
 char *owner;
 };
+
+/* New type name for struct dog */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
