fc4755845b422dc663cfe6f0d183c1731f848b60
Randa-hb10 <rm-400000@hotmail.com>
2025-11-06 13:31:50 +0000

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog _
+ * name for the dog 
+ * age the dog 
+ * owner the dog 
+ * description: use the struct for using name , age and owner the dog
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif
