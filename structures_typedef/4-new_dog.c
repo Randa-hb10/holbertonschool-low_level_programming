fa888bc7c182f41f609d1303f3e264c62116749e
Randa-hb10 <rm-400000@hotmail.com>
2025-11-06 14:58:37 +0000

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,24 +13,29 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *d;
+
 d = malloc(sizeof(dog_t));
 if (d == NULL)
 return (NULL);
+
 d->name = malloc(strlen(name) + 1);
 if (d->name == NULL)
 {
 free(d);
 return (NULL);
 }
+
 d->owner = malloc(strlen(owner) + 1);
 if (d->owner == NULL)
 {
-    free(d->name);
-    free(d);
-    return (NULL);
+free(d->name);
+free(d);
+return (NULL);
 }
+
 strcpy(d->name, name);
 strcpy(d->owner, owner);
 d->age = age;
+
 return (d);
 }
