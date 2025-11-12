57de6e70db57ae5a8c735f2aae9755d54da12248
Randa-hb10 <rm-400000@hotmail.com>
2025-11-12 13:25:55 +0000

    TASK 3 is done

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,54 +3,48 @@
 #include <stdarg.h>
 
 /**
- * print_all - prints anything based on format specifiers
+ * print_all - prints anything
  * @format: list of types of arguments passed to the function
  *
- * Description:
- * c: char
- * i: integer
- * f: float
- * s: string (if NULL, print (nil))
- * Any other character in format is ignored.
+ * Description: c = char, i = integer, f = float, s = string
+ * (nil) is printed if a string is NULL.
  */
 void print_all(const char * const format, ...)
 {
 va_list args;
 unsigned int i = 0;
-char *str;
-char *separator = "";
+char *str, *sep = "";
 
 va_start(args, format);
 
-while (format && format[i])
+if (format)
 {
-if (format[i] == 'c')
+while (format[i])
 {
-printf("%s%c", separator, va_arg(args, int));
-}
-else if (format[i] == 'i')
+switch (format[i])
 {
-printf("%s%d", separator, va_arg(args, int));
-}
-else if (format[i] == 'f')
-{
-printf("%s%f", separator, va_arg(args, double));
-}
-else if (format[i] == 's')
-{
+case 'c':
+printf("%s%c", sep, va_arg(args, int));
+break;
+case 'i':
+printf("%s%d", sep, va_arg(args, int));
+break;
+case 'f':
+printf("%s%f", sep, va_arg(args, double));
+break;
+case 's':
 str = va_arg(args, char *);
-if (str == NULL)
+if (!str)
 str = "(nil)";
-printf("%s%s", separator, str);
-}
-else
-{
+printf("%s%s", sep, str);
+break;
+default:
 i++;
 continue;
 }
-
-separator = ", ";
+sep = ", ";
 i++;
+}
 }
 
 printf("\n");
