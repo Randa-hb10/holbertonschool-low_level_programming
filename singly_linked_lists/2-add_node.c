212e2726ee67a8452e6a255ef6c208d916594adc
Randa-hb10 <rm-400000@hotmail.com>
2025-11-25 10:14:47 +0000

    Add add_node function to insert new node at beginning of list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,6 +13,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new_node;
+int len = 0;
 
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
@@ -24,7 +25,10 @@ if (new_node->str == NULL)
 free(new_node);
 return (NULL);
 }
-new_node->len = strlen(str);
+ while (str[len] != '\0')
+len++;
+new_node->len = len;
+
 new_node->next = *head;
 *head = new_node;
 
