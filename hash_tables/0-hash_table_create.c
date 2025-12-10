18289b59bc6bae7274699139b07e13c3153c3248
Randa-hb10 <rm-400000@hotmail.com>
2025-12-10 08:29:07 +0000

    fix files hash.h and 0 hash table create

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,28 +8,28 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table = NULL;
-    unsigned long int i;
+hash_table_t *new_table = NULL;
+unsigned long int i;
 
     /* Allocate memory for the hash table structure */
-    new_table = malloc(sizeof(hash_table_t));
-    if (new_table == NULL)
-        return (NULL);
+new_table = malloc(sizeof(hash_table_t));
+if (new_table == NULL)
+return (NULL);
 
     /* Set the size of the hash table */
-    new_table->size = size;
+new_table->size = size;
 
     /* Allocate memory for the array of pointers (buckets) */
-    new_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (new_table->array == NULL)
-    {
-        free(new_table);
-        return (NULL);
-    }
+new_table->array = malloc(sizeof(hash_node_t *) * size);
+if (new_table->array == NULL)
+{
+free(new_table);
+return (NULL);
+}
 
     /* Initialize all array elements to NULL */
-    for (i = 0; i < size; i++)
-        new_table->array[i] = NULL;
+for (i = 0; i < size; i++)
+new_table->array[i] = NULL;
 
-    return (new_table);
+return (new_table);
 }
