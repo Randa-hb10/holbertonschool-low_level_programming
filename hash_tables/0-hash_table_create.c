71d4ef912605024fe3fbb955a2f36172015f75d5
Randa-hb10 <rm-400000@hotmail.com>
2025-12-10 08:16:48 +0000

    Initial commit: Create hash table structure and implementation

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,36 +1,35 @@
-#include <stdlib.h>
 #include "hash_tables.h"
 
 /**
  * hash_table_create - Creates a hash table
  * @size: Size of the array
  *
- * Return: Pointer to newly created hash table, or NULL if fails
+ * Return: Pointer to the newly created hash table, or NULL if failed
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = NULL;
-	unsigned long int i;
+    hash_table_t *new_table = NULL;
+    unsigned long int i;
 
-	/* Allocate memory for hash table structure */
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-		return (NULL);
+    /* Allocate memory for the hash table structure */
+    new_table = malloc(sizeof(hash_table_t));
+    if (new_table == NULL)
+        return (NULL);
 
-	/* Allocate memory for array */
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-	{
-		free(ht);
-		return (NULL);
-	}
+    /* Set the size of the hash table */
+    new_table->size = size;
 
-	/* Initialize each element of array to NULL */
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+    /* Allocate memory for the array of pointers (buckets) */
+    new_table->array = malloc(sizeof(hash_node_t *) * size);
+    if (new_table->array == NULL)
+    {
+        free(new_table);
+        return (NULL);
+    }
 
-	/* Set size */
-	ht->size = size;
+    /* Initialize all array elements to NULL */
+    for (i = 0; i < size; i++)
+        new_table->array[i] = NULL;
 
-	return (ht);
+    return (new_table);
 }
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -2,7 +2,6 @@
 #define HASH_TABLES_H
 
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 
 /**
@@ -15,9 +14,9 @@
  */
 typedef struct hash_node_s
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
+    char *key;
+    char *value;
+    struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -30,11 +29,11 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-	unsigned long int size;
-	hash_node_t **array;
+    unsigned long int size;
+    hash_node_t **array;
 } hash_table_t;
 
-/* Function prototypes */
+/* Function prototype */
 hash_table_t *hash_table_create(unsigned long int size);
 
 #endif /* HASH_TABLES_H */
