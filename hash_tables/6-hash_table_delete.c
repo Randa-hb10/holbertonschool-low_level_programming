97a002cb2f48c0e4161d46292655f6a33b0e5a8c
Randa-hb10 <rm-400000@hotmail.com>
2025-12-12 18:18:08 +0000

    Implement hash_table_delete function to free all memory used by the hash table

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: Pointer to the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *temp;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			temp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = temp;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
+
